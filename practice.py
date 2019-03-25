8acddbdea73706d9ea96b8d4d0484b3e95d8ce78
tlstndus2560 <tlstndus2560@naver.com>
2019-03-25 20:25:44 +0900

    first commit

---
+++ practice.py
@@ -0,0 +1,10 @@
+
+
+lion_we_know = ['hyojin', 'b', 'c']
+lion_meet=['hyojin', 'b', 'd']
+
+for lion in lion_meet:
+    if lion in lion_we_know:
+        print("hello")
+    else:
+        print("I don't know")
